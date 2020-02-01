35add131ba306c1c03ddaee350ce92a207b2ac5f
vienpham2019 <vienpham2019@gmail.com>
2020-01-31 21:49:50 -0600

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+s1 = Student.new("new student")
+ 
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+puff.pass_student(patrick,"Power Steering 202")
+puff.pass_student(s1,"Power Steering 202")
+puff.fail_student(spongebob,"Don't Crash 101")
+krabs.pass_student(spongebob,"Major Test")
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
