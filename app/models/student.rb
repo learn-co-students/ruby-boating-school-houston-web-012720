e8592c3d42cbb16f7cad7e840aec089f9162b812
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-02 20:19:52 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,40 @@
 class Student
 
+    @@all = []
+
+    attr_reader :first_name
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(first_name)
+        all.find{|student| student.first_name == first_name}
+    end
+
+    def tests
+        BoatingTest.all.select{|test| test.student == self}
+    end
+
+    def tests_passed
+        tests.select{|test| test.test_status == "passed"}
+    end
+
+    def tests_failed
+        tests.select{|test| test.test_status == "failed"}
+    end
+
+    def grade_percentage
+        tests_passed.count/tests.count.to_f
+    end
+
 end
