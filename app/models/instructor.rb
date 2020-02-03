ede6fae74f75a33d04d060f5e78894de3304f2b0
GianMoran <gpmoran@uh.edu>
2020-02-03 09:27:53 -0600

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,19 @@
 class Instructor
-
+    @@all = []
+def initialize(name)
+@name = name 
+@@all << self 
+end
+def pass_student(student, test_name)
+test = BoatingTest.all.find{|test| test.student = student && test.test_name = test_name}
+test.status = "Passed"
+end
+def fail_student(student,test_name)
+test = BoatingTest.all.find{|test| test.student = student && test.test_name = test_name}
+test.status = "Failed"
+if test == nil 
+   new_test = BoatingTest.new(student, test_name, self )
+   new_test.status = "Failed"
+end
+end
 end
